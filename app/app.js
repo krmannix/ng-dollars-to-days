ba97782cf4468afa603fcdace49f425aadf42f8a
Kevin Mannix <kmannix@bu.edu>
2016-05-07 19:44:48 -0400

    remove boiler plate and add flat-ui

--- app/app.js
+++ app/app.js
@@ -3,10 +3,4 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+]);
